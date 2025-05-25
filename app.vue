0d4745afcd1544bd16e643f30cec110c84750b95
Pandau <pandau.ting@gmail.com>
2025-05-26 00:20:02 +0200

    connect to database

---
+++ app.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <p>hi</p>
+    <p>{{ products }}</p>
+    <NuxtRouteAnnouncer />
+    <NuxtWelcome />
+  </div>
+</template>
+
+<script setup>
+const products = await $fetch('/api/products')
+console.log(products)
+</script>
